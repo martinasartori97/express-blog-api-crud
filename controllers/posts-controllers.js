e18561f3f7dbd453e63468867424529d9f8bfdc9
martinasartori97 <martinasartori9@gmail.com>
2024-11-05 17:06:05 +0100

    edit js

--- controllers/posts-controllers.js
+++ controllers/posts-controllers.js
@@ -2,36 +2,58 @@
 const posts = require('../db.js')
 
 const index = (req, res) => {
-    res.json({ data: posts, count: posts.length })
-  }
+  res.json({ data: posts, count: posts.length })
+}
 
-  module.exports = {
-    index
-  }
+module.exports = {
+  index
+}
 
 
 
 const show = (req, res) => {
 
-    const posts = posts.find((posts) => posts.id === parseInt(req.params.id))
-    if (!posts) {
-      return res.status(404).json({ error: "No posts found with that id" })
-    }
-    return res.status(200).json({ data: posts })
+  const posts = posts.find((posts) => posts.id === parseInt(req.params.id))
+  if (!posts) {
+    return res.status(404).json({ error: "No posts found with that id" })
   }
+  return res.status(200).json({ data: posts })
+}
 
 
-  const store = (req,res) =>{
-    console.log(req.body);
-    res.json = ({
-      body: req.body
-    })
-    
+const store = (req, res) => {
 
-  }
+  // create the new post object
+  const post = {
+    title: "",
+    slug: "",
+    content: "",
+    image: "",
+    tags: ""
+  };
 
-  module.exports = {
-    index,
-    show,
-    store
-  }
+
+  return res.status(201).json({
+    status: 201,
+    data: menu,
+    count: menu.length
+  })
+
+
+
+
+
+  console.log(req.body);
+
+  res.json = ({
+    body: req.body
+  })
+
+
+}
+
+module.exports = {
+  index,
+  show,
+  store
+}
