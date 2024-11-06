ebcdc192e63eec9d93f0da7ade7576c8b90565c5
martinasartori97 <martinasartori9@gmail.com>
2024-11-06 15:14:06 +0100

    edit js

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,8 @@ const PostsController = require('../controllers/PostsController.js')
 router.get('/', PostsController.index)
 router.get('/:id', PostsController.show)
 router.post('/', PostsController.store)
+router.put("/:id", PizzaController.update)
+
 
 
 
