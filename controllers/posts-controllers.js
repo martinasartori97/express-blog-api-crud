f31983fb7a78ae8ce0e6e913ed7c648d67bf8610
martinasartori97 <martinasartori9@gmail.com>
2024-11-08 17:44:33 +0100

    edit js

--- controllers/posts-controllers.js
+++ controllers/posts-controllers.js
@@ -14,10 +14,10 @@ const show = (req, res) => {
   console.log(req.params.slug);
 
   const FoundPost = posts.find((post) => post.slug === req.params.slug)
-  if (!post) {
+  if (!FoundPost) {
     return res.status(404).json({ error: "No posts found with that slug " })
   }
-  return res.status(200).json({ data: posts })
+  return res.status(200).json({ data: FoundPost })
 }
 
 
@@ -52,15 +52,17 @@ const update = (req, res) => {
   console.log(req.params);
 
   const post = posts.find((post) => post.slug === req.params.slug);
-  if (!posts) {
-    return res.status(404).json({ error: "No post found with that slug" })
-  }
-  title: req.body.title,
-    slug: req.body.slug,
-      content: req.body.content,
-        image: req.body.image,
-          tags: req.body.tags
-  console.log(post);
+  console.log('+',post);
+  
+  // if (!post) {
+  //   return res.status(404).json({ error: "No post found with that slug" })
+  // }
+  // title: req.body.title,
+  //   slug: req.body.slug,
+  //     content: req.body.content,
+  //       image: req.body.image,
+  //         tags: req.body.tags
+  // console.log(post);
 
 }
 
