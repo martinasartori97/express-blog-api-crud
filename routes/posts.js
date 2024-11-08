26763a0c29b56736f6131672da5ee0db38de1ef5
martinasartori97 <martinasartori9@gmail.com>
2024-11-08 15:39:57 +0100

    edit js

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const PostsController = require('../controllers/posts-controllers.js')
 
 router.get('/', PostsController.index)
- router.get('/:slug', PostsController.show)
- router.post('/', PostsController.store)
+router.get('/:slug', PostsController.show)
+router.post('/', PostsController.store)
 // router.put("/:slug", PostsController.update)
 // router.delete("/:slug", PostsController.destroy)
 
